00d2d0dd0e7c418597af4224a531a756895ddbf1
Woo Sung Kang <geniusraban@gmail.com>
2022-11-09 19:27:40 +0900

    9

---
+++ leetcode/9.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x);
+        for i in range(len(x_str)):
+            if(x_str[i]!=x_str[len(x_str)-1-i]):
+                return False
+        return True
